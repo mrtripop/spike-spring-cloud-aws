95ae9d6c5fe62460c3e1b73dade205f7d3f0e2fe
mrtripop <tripop.chai12@gmail.com>
2023-10-13 00:48:36 +0700

    feat: initial spring boot project

---
+++ src/main/java/com/mrtripop/spike/springcloudaws/SpringCloudAwsApplication.java
@@ -0,0 +1,13 @@
+package com.mrtripop.spike.springcloudaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudAwsApplication.class, args);
+	}
+
+}
